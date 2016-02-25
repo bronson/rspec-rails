53571a4de75a8ec1312deeb94d55dfdc9d7a6acc
Scott Bronson <brons_github@rinspin.com>
2016-02-25 03:24:28 -0800

    let request specs use integration test services
    Rails integration tests have services like assert_select.
    This makes them available in request specs.

--- lib/rspec/rails/example/request_example_group.rb
+++ lib/rspec/rails/example/request_example_group.rb
@@ -1,5 +1,14 @@
 module RSpec
   module Rails
+    # Raises an error if the user tries to call integration test helpers.
+    # If the helpers are available then this module will be ignored.
+    module IntegrationHelperWarnings
+      extend ActiveSupport::Concern
+      [:assert_select, :url_for, :url_options].each do |name|
+        define_method(name) { |*| raise "you must upgrade Rails to call #{name}" }
+      end
+    end
+
     # @api public
     # Container class for request spec functionality.
     module RequestExampleGroup
@@ -10,6 +19,12 @@ module RSpec
       include RSpec::Rails::Matchers::RedirectTo
       include RSpec::Rails::Matchers::RenderTemplate
       include ActionController::TemplateAssertions
+
+      begin
+        include ActionDispatch::IntegrationTest::Behavior
+      rescue NameError
+        include IntegrationHelperWarnings
+      end
 
       # Delegates to `Rails.application`.
       def app
